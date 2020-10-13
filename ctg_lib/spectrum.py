5870921bb52ba3d87053f75b3c95b3b76f124e75
Petri Jehkonen <petri.jehkonen@aalto.fi>
2020-10-13 11:29:26 +0300

    Implemented most suitable windows.

--- ctg_lib/spectrum.py
+++ ctg_lib/spectrum.py
@@ -2,6 +2,19 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
+# Create np array with each row presenting a windowing vector length v_len
+def make_windows(v_len):
+    win_name = ['blackman', 'hamming','hann','flattop', 'blackmanharris', 'tukey']
+    windows = []
+
+    for window in win_name[:-1]:
+        windows.append(signal.get_window(window, v_len))
+
+    windows.append(signal.windows.tukey(v_len, alpha=0.1))
+
+    return np.array(windows), win_name
+
+
 def make_spectrogram(pdg, df):
     results = np.empty(df.shape[1])
     for column in df.columns:
@@ -21,10 +34,15 @@ def make_spectrogram(pdg, df):
 
 def make_welch_psd(pdg, df):
     fs = 4.0
-    window='hann'
+    v_len = df.shape[0]
+
+    windows, win_names = make_windows(v_len)
+
+    print(windows)
+    print(win_names)
     results = np.empty(df.shape[1])
     for column in df.columns:
-        freqs, psd = signal.welch(df[column].values)
+        freqs, psd = signal.welch(df[column].values, fs=fs, )
         plt.figure(figsize=(5, 4))
         plt.semilogx(freqs, psd)
         plt.title('PSD: power spectral density of column {}'.format(column))
