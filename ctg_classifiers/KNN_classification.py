781c135e8da1985c79e7e4f190526cf070aa6bcc
Petri Jehkonen <petri.jehkonen@aalto.fi>
2020-10-23 16:25:46 +0300

    KNN works with subset of data.

--- ctg_classifiers/KNN_classification.py
+++ ctg_classifiers/KNN_classification.py
@@ -6,18 +6,18 @@ from sklearn.metrics import confusion_matrix
 
 import os
 
-def CTG_KNN(X_train, y_train, X_test, y_test):
+def CTG_KNN(X_train, X_test, y_train, y_test):
     nn_neighbors = 5
     os.environ['OMP_NUM_THREADS'] = '16'
     nn_jobs = -1
     knn = KNeighborsClassifier(n_neighbors=nn_neighbors, n_jobs=nn_jobs)
-    knn.fit(X_train, y_train.values.ravel())
+    knn.fit(X_train, y_train)
 
     y_pred = knn.predict(X_test)
 
-    print("Accuracy is ",knn.score(X_test, y_test.values.ravel()))
+    print("Accuracy is ",knn.score(X_test, y_test))
     print("Confusion matrix")
-    print(confusion_matrix(y_test.values.ravel(), y_pred))
+    print(confusion_matrix(y_test, y_pred))
     print("Classification report")
-    print(classification_report(y_test.values.ravel(), y_pred))
+    print(classification_report(y_test, y_pred))
     
